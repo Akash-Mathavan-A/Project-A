9e23a4154a3d48e255c3e6c67f11367f1fe21c2e
Akash-Mathavan-A <akashmathavana@gmail.com>
2024-07-24 12:54:32 +0530

    feat : value changed

--- AddingNumber.java
+++ AddingNumber.java
@@ -1,7 +1,7 @@
 public class AddingNumber {
     public static void main(String[] args) {
         int num1 = 19;
-        int num2 = -100;
+        int num2 = 564;
         int result = sum(num1 , num2);
         System.out.println("Sum is : " + result);    
     }
