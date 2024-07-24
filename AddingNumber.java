0829656957ce896dd585c14e61896210ffee6ee4
Akash-Mathavan-A <akashmathavana@gmail.com>
2024-07-24 12:06:05 +0530

    fix : Using method for readable code

--- AddingNumber.java
+++ AddingNumber.java
@@ -2,7 +2,10 @@ public class AddingNumber {
     public static void main(String[] args) {
         int num1 = 19;
         int num2 = -35;
-        int sum = num1 + num2;
-        System.out.println("Sum is : " + sum);    
+        int result = sum(num1 , num2);
+        System.out.println("Sum is : " + result);    
+    }
+    public static int sum(int a , int b){
+        return a + b;
     }
 }
