36d17ef0990195136646349f5d07d45c8fd549a0
Akash-Mathavan-A <akashmathavana@gmail.com>
2024-07-24 12:27:11 +0530

    feat : add some lines

--- CountDigits.java
+++ CountDigits.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class CountDigits{
     static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
+        System.out.println("Enter the Number : ");
         int number = in.nextInt();
         int result = countDigit(number);
         System.out.println("Result is : " + result);
