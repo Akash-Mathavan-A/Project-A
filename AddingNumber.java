e6f1b8a348993a744cf1318c0248d854d7bef59f
Akash-Mathavan-A <akashmathavana@gmail.com>
2024-07-24 12:02:36 +0530

    feat : created Add Two Numbers java file

---
+++ AddingNumber.java
@@ -0,0 +1,8 @@
+public class AddingNumber {
+    public static void main(String[] args) {
+        int num1 = 19;
+        int num2 = -35;
+        int sum = num1 + num2;
+        System.out.println("Sum is : " + sum);    
+    }
+}
