621539619bb89a8170356bca03d3013cb77ae67d
Akash-Mathavan-A <akashmathavana@gmail.com>
2024-07-24 12:16:27 +0530

    feat : Modify by Single Line code

--- CountDigits.java
+++ CountDigits.java
@@ -2,18 +2,11 @@ import java.util.*;
 public class CountDigits{
     static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-        //Input get from Keyboard
         int number = in.nextInt();
-        
         int result = countDigit(number);
         System.out.println("Result is : " + result);
     }
     public static int countDigit(int number){
-        int count = 0;
-        while(number > 0){
-            number /= 10;
-            count += 1;
-        }
-        return count;
+        return (int)(Math.log10(number)) + 1;
     }
 }
